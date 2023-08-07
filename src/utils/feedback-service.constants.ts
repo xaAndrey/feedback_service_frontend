180b3124a3a8578775308d3f5fb873c47c31798e
andreypisarev <xaandreynx@gmail.com>
2023-08-07 15:59:47 +0300

    [feat]<constants>: add constants for web project

---
+++ src/utils/feedback-service.constants.ts
@@ -0,0 +1,7 @@
+export const BACKEND_URL = '/api/v1/';
+export const AUTH_BACKEND_URL = '/auth/';
+
+export const ACCESS_TOKEN_COOKIE_KEY = 'accessToken';
+export const ACCESS_TOKEN_EXPIRATION_TIME_COOKIE_KEY = 'accessTokenExpirationTime';
+export const REFRESH_TOKEN_COOKIE_KEY = 'refreshToken';
+export const REFRESH_TOKEN_EXPIRATION_TIME_COOKIE_KEY = 'refreshTokenExpirationTime';
