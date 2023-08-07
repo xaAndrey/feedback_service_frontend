08ce970d548842458b53ab2a3c2a9e33243c7758
andreypisarev <xaandreynx@gmail.com>
2023-08-07 15:59:15 +0300

    [feat]<style>: create theme

---
+++ src/helpers/theme.ts
@@ -0,0 +1,16 @@
+import {createTheme} from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#2A8C7A',
+            dark: '#8A388C'
+        },
+        secondary: {
+            main: '#D9BD89'
+        },
+        background:{
+            paper: '#fff7ea'
+        }
+    }
+})
