1436e00300fc48ca9cab5440d7cc658bd843fb58
andreypisarev <xaandreynx@gmail.com>
2023-08-07 16:35:10 +0300

    [feat]: rewrote App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useRoutes } from "./hooks/useRoutes";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, createStore } from "@reduxjs/toolkit";
+import thunkMiddleware from "redux-thunk";
+import { rootReducer } from "./redux";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./helpers/theme";
 
-const App = () => {
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunkMiddleware
+    )
+  )
+);
+
+function App() {
+  const routes: JSX.Element = useRoutes();
+
   return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          {routes}
+        </Router>
+      </ThemeProvider>
+    </Provider>
+  )
 }
 
 export default App;
