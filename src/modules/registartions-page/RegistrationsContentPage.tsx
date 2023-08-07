3c9ec23c416182f334c98f8e5fc5603ce21dbbd7
andreypisarev <xaandreynx@gmail.com>
2023-08-07 16:29:38 +0300

    [feat]<modules>: add RegistrationsContentPage for Registration module

---
+++ src/modules/registartions-page/RegistrationsContentPage.tsx
@@ -0,0 +1,54 @@
+import { Grid, Stack, Typography, ToggleButton, Box, CircularProgress, Dialog } from "@mui/material";
+import { RegistrationDto } from "../../api/registrations/dto";
+import React, { useState } from "react";
+import { RegistrationsTable } from "./RegistrationsTable";
+// import {differenceInDays, parseISO} from "date-fns";
+import {orderBy} from "lodash";
+
+export interface IRegistrationsContentPage {
+    registrations: RegistrationDto[],
+    isFetching: boolean
+}
+
+export const RegistrationsContentPage: React.FC<IRegistrationsContentPage> = ({ registrations, isFetching }) => {
+    const [columnToSort, setColumnToSort] = useState('id');
+    const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>("desc");
+
+    const invertDirection = {
+        asc: "desc",
+        desc: "asc"
+    }
+
+    const handleSort = (columnName: string) => {
+        console.log(columnName, columnToSort, sortDirection);
+        setColumnToSort(columnName);
+        setSortDirection(columnToSort === columnName ? (invertDirection as any)[sortDirection] : 'asc')
+    }
+
+    return <Stack m={5}>
+        <Grid item={true} xs={8}>
+            <Grid container={true} width={'100%'} direction={'row'} mt={3} spacing={2}>
+                <Grid item={true} xs={9}>
+                    <Typography fontSize={24} color={'#1D1929'} fontWeight={700}>Регистрации на прием</Typography>
+                </Grid>
+            </Grid>
+            <Grid container={true} width={'100%'} direction={'row'} spacing={2}>
+                <Grid item={true} xs={12}>
+                    {isFetching ?
+                        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <CircularProgress />
+                        </Box> :
+                        <>
+                            {<RegistrationsTable 
+                                registrations={orderBy(registrations, columnToSort, sortDirection)}
+                                handleSort={handleSort}
+                                columnToSort={columnToSort}
+                                sortDirection={sortDirection} /> 
+                                }
+                        </>
+                    }
+                </Grid>
+            </Grid>
+        </Grid>
+    </Stack>
+}
